c0eb7426c2e89b319cac180444de26538ef57178
Jaguar1225 <95676529+Jaguar1225@users.noreply.github.com>
2025-03-26 10:03:09 +0900

    Small change in data path

--- utils/dataloader/train_data_set.py
+++ utils/dataloader/train_data_set.py
@@ -2,6 +2,6 @@ from .loader import Data_Loader
 
 class Train_Data_Set(Data_Loader):
     def __init__(self):
-        self.data_path = 'data/train_data.csv'
+        self.data_path = 'data/train/'
         super(Train_Data_Set, self).__init__(self.data_path)
     
