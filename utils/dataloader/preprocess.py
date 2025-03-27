64c5686787681c805f5c5f051f34ed7ad2d4eb5b
Jaguar1225 <95676529+Jaguar1225@users.noreply.github.com>
2025-03-27 11:53:04 +0900

    Again debugging

--- utils/dataloader/preprocess.py
+++ utils/dataloader/preprocess.py
@@ -7,22 +7,34 @@ class Normalize:
         self.params = params
 
     def standardize(self, data):
-        return (data - data.mean(axis=-1,keepdims=True))/(data.std(axis=-1,keepdims=True))
+        if data is torch.Tensor:
+            return (data - data.mean(dim=-1,keepdim=True))/(data.std(dim=-1,keepdim=True))
+        else:
+            return (data - data.mean(axis=-1,keepdims=True))/(data.std(axis=-1,keepdims=True))
     
     def minmax(self, data):
-        return (data - data.min(axis=-1,keepdims=True))/(data.max(axis=-1,keepdims=True)-data.min(axis=-1,keepdims=True))
+        if data is torch.Tensor:
+            return (data - data.min(dim=-1,keepdim=True))/(data.max(dim=-1,keepdim=True)-data.min(dim=-1,keepdim=True))
+        else:
+            return (data - data.min(axis=-1,keepdims=True))/(data.max(axis=-1,keepdims=True)-data.min(axis=-1,keepdims=True))
     
     def maxabs(self, data):
-        return data/data.max(axis=-1,keepdims=True)
+        if data is torch.Tensor:
+            return data/data.max(dim=-1,keepdim=True)
+        else:
+            return data/data.max(axis=-1,keepdims=True)
     
     def partial(self, data):
-        try:
-            return data/np.sum(data,axis=-1,keepdims=True)
-        except:
+        if data is torch.Tensor:
             return data/torch.sum(data,dim=-1,keepdims=True)
+        else:
+            return data/np.sum(data,axis=-1,keepdims=True)
         
     def partial_normalize(self, data, window_size=10):
-        return data/np.sum(data,axis=-1,keepdims=True)
+        if data is torch.Tensor:
+            return data/torch.sum(data,dim=-1,keepdims=True)
+        else:
+            return data/np.sum(data,axis=-1,keepdims=True)
 
 
     
